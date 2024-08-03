e2f9385c8b2d2f9160d586c27f717e6c9aaa7f15
Ihsan Ullah <68999003+ihsan-buneri@users.noreply.github.com>
2024-08-03 08:59:38 +0500

    Add content to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
 const Home = () => {
-	return (
-		<div className='home'>
-			<section></section>
-		</div>
-	);
+  return (
+    <div className="home">
+      <section></section>
+
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
+
+	  <main>
+
+	  </main>
+    </div>
+  );
 };
 
 export default Home;
