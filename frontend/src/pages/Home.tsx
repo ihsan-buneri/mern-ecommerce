c8adde8a964b74bc95b9d66985c8db1b3ca28e7f
Ihsan Ullah <68999003+ihsan-buneri@users.noreply.github.com>
2024-08-03 11:24:44 +0500

    Add Product card

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,30 @@
 import { Link } from "react-router-dom";
+import ProductCard from "../components/ProductsCard";
 const Home = () => {
-  return (
-    <div className="home">
-      <section></section>
+	const addToCardHandler = () => {};
+	return (
+		<div className='home'>
+			<section></section>
 
-      <h1>
-        Latest Products
-        <Link to="/search" className="findmore">
-          More
-        </Link>
-      </h1>
+			<h1>
+				Latest Products
+				<Link to='/search' className='findmore'>
+					More
+				</Link>
+			</h1>
 
-	  <main>
-
-	  </main>
-    </div>
-  );
+			<main>
+				<ProductCard
+					productId='addd'
+					name='Macbook'
+					price={4545}
+					stock={433}
+					handler={addToCardHandler}
+					photo='https://m.media-amazon.com/images/I/41DeT328kPL._AC_SX679_.jpg'
+				/>
+			</main>
+		</div>
+	);
 };
 
 export default Home;
