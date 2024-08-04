5e6044a1a4d3b1c18fa21604952578cf1174fac4
Ihsan Ullah <68999003+ihsan-buneri@users.noreply.github.com>
2024-08-04 12:49:38 +0500

    Frontend Initial UI Complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
-import Loader from "./components/Loader";
+import { onAuthStateChanged } from "firebase/auth";
+import { Suspense, lazy, useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Loader, { LoaderLayout } from "./components/Loader";
+import ProtectedRoute from "./components/protected-route";
+import { auth } from "./firebase";
+import { getUser } from "./redux/api/userAPI";
+import { userExist, userNotExist } from "./redux/reducer/userReducer";
+import { RootState } from "./redux/store";
+import Footer from "./components/footer";
+
 const Home = lazy(() => import("./pages/Home"));
 const Search = lazy(() => import("./pages/Search"));
+const ProductDetails = lazy(() => import("./pages/product-details"));
 const Cart = lazy(() => import("./pages/Cart"));
+const Shipping = lazy(() => import("./pages/Shipping"));
+const Login = lazy(() => import("./pages/Login"));
+const Orders = lazy(() => import("./pages/Orders"));
+const OrderDetails = lazy(() => import("./pages/OrderDetails"));
+const NotFound = lazy(() => import("./pages/not-found"));
+const Checkout = lazy(() => import("./pages/checkout"));
 
-// Admin routes importing
+// Admin Routes Importing
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
 const Products = lazy(() => import("./pages/admin/products"));
 const Customers = lazy(() => import("./pages/admin/customers"));
 const Transaction = lazy(() => import("./pages/admin/transaction"));
+const Discount = lazy(() => import("./pages/admin/discount"));
 const Barcharts = lazy(() => import("./pages/admin/charts/barcharts"));
 const Piecharts = lazy(() => import("./pages/admin/charts/piecharts"));
 const Linecharts = lazy(() => import("./pages/admin/charts/linecharts"));
@@ -24,26 +42,74 @@ const ProductManagement = lazy(
 const TransactionManagement = lazy(
 	() => import("./pages/admin/management/transactionmanagement")
 );
+const DiscountManagement = lazy(
+	() => import("./pages/admin/management/discountmanagement")
+);
 
-function App() {
-	return (
+const NewDiscount = lazy(() => import("./pages/admin/management/newdiscount"));
+
+const App = () => {
+	const { user, loading } = useSelector(
+		(state: RootState) => state.userReducer
+	);
+
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		onAuthStateChanged(auth, async (user) => {
+			if (user) {
+				const data = await getUser(user.uid);
+				dispatch(userExist(data.user));
+			} else dispatch(userNotExist());
+		});
+	}, []);
+
+	return loading ? (
+		<Loader />
+	) : (
 		<Router>
-			<Suspense fallback={<Loader />}>
-				<Header />
+			{/* Header */}
+			<Header user={user} />
+			<Suspense fallback={<LoaderLayout />}>
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/search' element={<Search />} />
+					<Route path='/product/:id' element={<ProductDetails />} />
 					<Route path='/cart' element={<Cart />} />
-					{/* Admin Routes  */}
+					{/* Not logged In Route */}
 					<Route
-					//   element={
-					//     <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
-					//   }
+						path='/login'
+						element={
+							<ProtectedRoute isAuthenticated={user ? false : true}>
+								<Login />
+							</ProtectedRoute>
+						}
+					/>
+					{/* Logged In User Routes */}
+					<Route
+						element={<ProtectedRoute isAuthenticated={user ? true : false} />}
+					>
+						<Route path='/shipping' element={<Shipping />} />
+						<Route path='/orders' element={<Orders />} />
+						<Route path='/order/:id' element={<OrderDetails />} />
+						<Route path='/pay' element={<Checkout />} />
+					</Route>
+					{/* Admin Routes */}
+					<Route
+						element={
+							<ProtectedRoute
+								isAuthenticated={true}
+								adminOnly={true}
+								admin={user?.role === "admin" ? true : false}
+							/>
+						}
 					>
 						<Route path='/admin/dashboard' element={<Dashboard />} />
 						<Route path='/admin/product' element={<Products />} />
 						<Route path='/admin/customer' element={<Customers />} />
 						<Route path='/admin/transaction' element={<Transaction />} />
+						<Route path='/admin/discount' element={<Discount />} />
+
 						{/* Charts */}
 						<Route path='/admin/chart/bar' element={<Barcharts />} />
 						<Route path='/admin/chart/pie' element={<Piecharts />} />
@@ -62,12 +128,22 @@ function App() {
 							path='/admin/transaction/:id'
 							element={<TransactionManagement />}
 						/>
+
+						<Route path='/admin/discount/new' element={<NewDiscount />} />
+
+						<Route
+							path='/admin/discount/:id'
+							element={<DiscountManagement />}
+						/>
 					</Route>
-					;
+
+					<Route path='*' element={<NotFound />} />
 				</Routes>
 			</Suspense>
+			<Footer />
+			<Toaster position='bottom-center' />
 		</Router>
 	);
-}
+};
 
 export default App;
